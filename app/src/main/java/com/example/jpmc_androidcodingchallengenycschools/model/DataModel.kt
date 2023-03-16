fdca9f35d3d23f77b59c90e611050f8a6b13167e
jonathan.diaz <jonathan.diaz@accenture.com>
2023-03-16 12:57:01 -0400

    added

--- app/src/main/java/com/example/jpmc_androidcodingchallengenycschools/model/DataModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.jpmc_androidcodingchallengenycschools.model
-
-// Contains the data we want to show on UI (in MainFragment)
-data class DataModel(val textForUI: String)
---
+++ app/src/main/java/com/jonathandiaz/jpmc_androidcodingchallengenycschools/model/DataModel.kt
@@ -0,0 +1,4 @@
+package com.jonathandiaz.jpmc_androidcodingchallengenycschools.model
+
+// Contains the data we want to show on UI (in MainFragment)
+data class DataModel(val textForUI: String)
